cfba813a5fc47ace978068bfba287866a36ba5bb
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2023-03-15 08:19:57 -0600

    Cocodrilos funcionando

--- app/src/main/java/com/example/appreptiles/Cocodrilos2.java
+++ app/src/main/java/com/example/appreptiles/Cocodrilos2.java
@@ -6,31 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Cocodrilo extends AppCompatActivity {
+public class Cocodrilos2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cocodriolos);
+        setContentView(R.layout.activity_cocodrilos2);
     }
 
     public void goMain(View view){
-        Intent i = new Intent( Cocodrilo.this, MainActivity.class);
+        Intent i = new Intent( Cocodrilos2.this, MainActivity.class);
         startActivity(i);
     }
 
     public void goCoc(View view){
-        Intent i = new Intent( Cocodrilo.this, Cocodrilo.class);
+        Intent i = new Intent( Cocodrilos2.this, Cocodrilo.class);
         startActivity(i);
     }
 
     public void goCoc1(View view){
-        Intent i = new Intent( Cocodrilo.this, Cocodrilos2.class);
+        Intent i = new Intent( Cocodrilos2.this, Cocodrilos2.class);
         startActivity(i);
     }
 
     public void goCoc2(View view){
-        Intent i = new Intent( Cocodrilo.this, Cocodrilos3.class);
+        Intent i = new Intent( Cocodrilos2.this, Cocodrilos3.class);
         startActivity(i);
     }
 }
--- app/src/main/java/com/example/appreptiles/Cocodrilos3.java
+++ app/src/main/java/com/example/appreptiles/Cocodrilos3.java
@@ -6,31 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Cocodrilo extends AppCompatActivity {
+public class Cocodrilos3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cocodriolos);
+        setContentView(R.layout.activity_cocodrilos3);
     }
 
     public void goMain(View view){
-        Intent i = new Intent( Cocodrilo.this, MainActivity.class);
+        Intent i = new Intent( Cocodrilos3.this, MainActivity.class);
         startActivity(i);
     }
 
     public void goCoc(View view){
-        Intent i = new Intent( Cocodrilo.this, Cocodrilo.class);
+        Intent i = new Intent( Cocodrilos3.this, Cocodrilo.class);
         startActivity(i);
     }
 
     public void goCoc1(View view){
-        Intent i = new Intent( Cocodrilo.this, Cocodrilos2.class);
+        Intent i = new Intent( Cocodrilos3.this, Cocodrilos2.class);
         startActivity(i);
     }
 
     public void goCoc2(View view){
-        Intent i = new Intent( Cocodrilo.this, Cocodrilos3.class);
+        Intent i = new Intent( Cocodrilos3.this, Cocodrilos3.class);
         startActivity(i);
     }
 }
