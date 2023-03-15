6ed3c4024a61ee84065f3cb57f1660f47121e529
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2023-03-15 07:46:39 -0600

    Squamatas funcionando

--- app/src/main/java/com/example/appreptiles/Squamatas3.java
+++ app/src/main/java/com/example/appreptiles/Squamatas3.java
@@ -6,31 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Squamatas extends AppCompatActivity {
+public class Squamatas3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_squamatas);
+        setContentView(R.layout.activity_squamatas3);
     }
 
     public void goMain(View view){
-        Intent i = new Intent( Squamatas.this, MainActivity.class);
+        Intent i = new Intent( Squamatas3.this, MainActivity.class);
         startActivity(i);
     }
 
     public void goSqa(View view){
-        Intent i = new Intent( Squamatas.this, Squamatas.class);
+        Intent i = new Intent( Squamatas3.this, Squamatas.class);
         startActivity(i);
     }
 
     public void goSqa1(View view){
-        Intent i = new Intent( Squamatas.this, Squamatas2.class);
+        Intent i = new Intent( Squamatas3.this, Squamatas2.class);
         startActivity(i);
     }
 
     public void goSqa2(View view){
-        Intent i = new Intent( Squamatas.this, Squamatas3.class);
+        Intent i = new Intent( Squamatas3.this, Squamatas3.class);
         startActivity(i);
     }
 }
